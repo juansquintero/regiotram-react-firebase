f008573bf199d2e36126f5710b2026b398792da5
Juan Sebastian Quintero <strangelife28@gmail.com>
2022-05-05 15:12:11 -0500

    setup data

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,14 +1,28 @@
-import * as React from 'react';
+//import * as React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { AuthenticatedUserContext } from '../providers';
 import { HomeScreen } from '../screens';
+import { AdminScreen, UserScreen, CashierScreen } from '../screens';
 
 const Stack = createStackNavigator();
 
 export const AppStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={HomeScreen} />
-    </Stack.Navigator>
+  const { user } = useContext(AuthenticatedUserContext);
+  if (user.rol === 1) {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name='Admin' component={AdminScreen} />              
+      </Stack.Navigator>
+    );
+  }else{
+      <Stack.Navigator>
+        <Stack.Screen name='User' component={UserScreen} />      
+      </Stack.Navigator>
+  }
+  return (      
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />       
+      </Stack.Navigator>
   );
 };
